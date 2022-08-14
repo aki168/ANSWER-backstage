82ac0cdea7b01d2ec722a3e4678c94145be1ba57
aki <ahsi.aki@gmail.com>
2022-08-15 01:22:56 +0800

    navbar style update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,62 +1,74 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import Icon from '@mdi/react'
+import { mdiChartBox, mdiForum, mdiClipboardText, mdiPlayBoxMultiple, mdiAccountMultiple, mdiCog  } from '@mdi/js';
 
 
 export default function Navbar() {
+
+  const path = 'https://raw.githubusercontent.com/aki168/ANSWER-backstage/main/public/images/';
+
   return (//flex-shrink-0
-    <nav className="d-flex flex-column col-3 p-3 bg-light vh-100" >
-    <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      <svg className="bi me-2" width="40" height="32"><Link to="#bootstrap"></Link></svg>
-      <span className="fs-4">Sidebar</span>
-    </Link>
-    <hr/>
-    <ul className="nav nav-pills flex-column mb-auto">
-      <li className="nav-item">
-        <Link to="admin" className="nav-link active" aria-current="page">
-          <svg className="bi me-2" width="16" height="16"><Link to="#home"></Link></svg>
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="admin" className="nav-link link-dark">
-          <svg className="bi me-2" width="16" height="16"><Link to="#speedometer2"></Link></svg>
-          Dashboard
-        </Link>
-      </li>
-      <li>
-        <Link to="/" className="nav-link link-dark">
-          <svg className="bi me-2" width="16" height="16"><Link to="#table"></Link></svg>
-          Orders
-        </Link>
-      </li>
-      <li>
-        <Link to="/" className="nav-link link-dark">
-          <svg className="bi me-2" width="16" height="16"><Link to="#grid"></Link></svg>
-          Products
-        </Link>
-      </li>
-      <li>
-        <Link to="/" className="nav-link link-dark">
-          <svg className="bi me-2" width="16" height="16"><Link to="#people-circle"></Link></svg>
-          Customers
-        </Link>
-      </li>
-    </ul>
-    <hr/>
-    <div className="dropdown">
-      <Link to="#" className="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
-        <strong>mdo</strong>
+    <nav className="d-flex flex-column col-3 py-8 ps-0 bg-light vh-100" >
+
+      <Link to="/" className="d-flex align-items-center mb-8 ps-8">
+        <img src="images/logo-ANSWER.svg" alt="LOGO" />
       </Link>
-      <ul className="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-        <li><Link className="dropdown-item" to="#">New project...</Link></li>
-        <li><Link className="dropdown-item" to="#">Settings</Link></li>
-        <li><Link className="dropdown-item" to="#">Profile</Link></li>
-        <li><hr className="dropdown-divider" /></li>
-        <li><Link className="dropdown-item" to="#">Sign out</Link></li>
+
+      <ul className="nav nav-pills flex-column mb-auto">
+
+        <li className="nav-item border-start border-4 border-light mb-6">
+          <Link to="admin" className="nav-link fw-bold link-dark ps-8" aria-current="page">
+            <Icon path={mdiChartBox} size={1} className="text-dark me-4" />
+            Dashboard
+          </Link>
+        </li>
+
+        <li className="nav-item border-start border-4 border-light mb-6">
+          <Link to="admin" className="nav-link fw-bold link-dark ps-8" aria-current="page">
+            <Icon path={mdiForum} size={1} className="text-dark me-4" />
+            Question
+          </Link>
+        </li>
+
+        <li className="nav-item border-start border-4 border-success mb-6">
+          <Link to="/" className="nav-link link-success fw-bold ps-8" aria-current="page">
+            <Icon path={mdiClipboardText} size={1} className="text-success me-4" />
+            Assignment
+          </Link>
+        </li>
+
+        <li className="nav-item border-start border-4 border-light mb-6">
+          <Link to="admin" className="nav-link fw-bold link-dark ps-8" aria-current="page">
+            <Icon path={mdiPlayBoxMultiple} size={1} className="text-dark me-4" />
+            Course
+          </Link>
+        </li>
+
+        <li className="nav-item border-start border-4 border-light mb-6">
+          <Link to="admin" className="nav-link fw-bold link-dark ps-8" aria-current="page">
+            <Icon path={mdiAccountMultiple} size={1} className="text-dark me-4" />
+            Admin
+          </Link>
+        </li>
+
+        <li className="nav-item border-start border-4 border-light mb-6">
+          <Link to="admin" className="nav-link fw-bold link-dark ps-8" aria-current="page">
+            <Icon path={mdiCog} size={1} className="text-dark me-4" />
+            Settings
+          </Link>
+        </li>
+
       </ul>
-    </div>
-  </nav>
+
+      <div className="ps-8 d-flex">
+          <img src="images/andychen.jpeg" alt="user" width="40" height="40" className="rounded-circle me-2" />
+          <div className="ms-4">
+            <strong>Andy Chen</strong>
+            <p className="text-secondary">andy0401@mail.com</p> 
+          </div>
+      </div>
+    </nav>
   )
 }
 
