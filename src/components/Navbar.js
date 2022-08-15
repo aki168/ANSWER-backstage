6c133a0e8758a06ef7f371fa94e851a2ffa37a4c
aki <ahsi.aki@gmail.com>
2022-08-15 19:19:24 +0800

    list table / conditinal style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,23 @@ import React from "react"
 import { Link } from "react-router-dom"
 import Icon from '@mdi/react'
 import { mdiChartBox, mdiForum, mdiClipboardText, mdiPlayBoxMultiple, mdiAccountMultiple, mdiCog  } from '@mdi/js';
+import { useState } from "react";
+
+
 
 
 export default function Navbar() {
 
   const path = 'https://raw.githubusercontent.com/aki168/ANSWER-backstage/main/public/images/';
 
+  const [toggleState, setToggleState] = useState("assignment");
+
+  const addStyle = (index) => {
+    setToggleState(index);
+  }
+
+  
+  
   return (//flex-shrink-0
     <nav className="d-flex flex-column col-3 py-8 ps-0 bg-light vh-100" >
 
@@ -17,44 +28,50 @@ export default function Navbar() {
 
       <ul className="nav nav-pills flex-column mb-auto">
 
-        <li className="nav-item border-start border-4 border-light mb-6">
-          <Link to="admin" className="nav-link fw-bold link-dark ps-8" aria-current="page">
-            <Icon path={mdiChartBox} size={1} className="text-dark me-4" />
+        <li className={`nav-item border-start border-4 border-${toggleState==="dashboard" ? "success" : "light" } mb-6 `} 
+          onClick={() => addStyle("dashboard")}>
+          <Link to="admin" className={`nav-link fw-bold link-${toggleState==="dashboard" ? "success" : "dark" } ps-8`} aria-current="page">
+            <Icon path={mdiChartBox} size={1} className="me-4" />
             Dashboard
           </Link>
         </li>
 
-        <li className="nav-item border-start border-4 border-light mb-6">
-          <Link to="admin" className="nav-link fw-bold link-dark ps-8" aria-current="page">
-            <Icon path={mdiForum} size={1} className="text-dark me-4" />
+        <li className={`nav-item border-start border-4 border-${toggleState==="question" ? "success" : "light" } mb-6`} 
+          onClick={() => addStyle("question")}>
+          <Link to="/" className={`nav-link fw-bold link-${toggleState==="question" ? "success" : "dark" } ps-8`} aria-current="page">
+            <Icon path={mdiForum} size={1} className="me-4" />
             Question
           </Link>
         </li>
 
-        <li className="nav-item border-start border-4 border-success mb-6">
-          <Link to="/" className="nav-link link-success fw-bold ps-8" aria-current="page">
-            <Icon path={mdiClipboardText} size={1} className="text-success me-4" />
+        <li className={`nav-item border-start border-4 border-${toggleState==="assignment" ? "success" : "light" } mb-6`} 
+          onClick={() => addStyle("assignment")}>
+          <Link to="/" className={`nav-link fw-bold link-${toggleState==="assignment" ? "success" : "dark" } ps-8`} aria-current="page">
+            <Icon path={mdiClipboardText} size={1} className="me-4" />
             Assignment
           </Link>
         </li>
 
-        <li className="nav-item border-start border-4 border-light mb-6">
-          <Link to="admin" className="nav-link fw-bold link-dark ps-8" aria-current="page">
-            <Icon path={mdiPlayBoxMultiple} size={1} className="text-dark me-4" />
+        <li className={`nav-item border-start border-4 border-${toggleState==="course" ? "success" : "light" } mb-6`}  
+          onClick={() => addStyle("course")}>
+          <Link to="admin" className={`nav-link fw-bold link-${toggleState==="course" ? "success" : "dark" } ps-8`} aria-current="page">
+            <Icon path={mdiPlayBoxMultiple} size={1} className="me-4" />
             Course
           </Link>
         </li>
 
-        <li className="nav-item border-start border-4 border-light mb-6">
-          <Link to="admin" className="nav-link fw-bold link-dark ps-8" aria-current="page">
-            <Icon path={mdiAccountMultiple} size={1} className="text-dark me-4" />
+        <li className={`nav-item border-start border-4 border-${toggleState==="admin" ? "success" : "light" } mb-6`}  
+          onClick={() => addStyle("admin")}>
+          <Link to="admin" className={`nav-link fw-bold link-${toggleState==="admin" ? "success" : "dark" } ps-8`} aria-current="page">
+            <Icon path={mdiAccountMultiple} size={1} className="me-4" />
             Admin
           </Link>
         </li>
 
-        <li className="nav-item border-start border-4 border-light mb-6">
-          <Link to="admin" className="nav-link fw-bold link-dark ps-8" aria-current="page">
-            <Icon path={mdiCog} size={1} className="text-dark me-4" />
+        <li className={`nav-item border-start border-4 border-${toggleState==="settings" ? "success" : "light" } mb-6`}  
+          onClick={() => addStyle("settings")}>
+          <Link to="/" className={`nav-link fw-bold link-${toggleState==="settings" ? "success" : "dark" } ps-8`} aria-current="page">
+            <Icon path={mdiCog} size={1} className="me-4" />
             Settings
           </Link>
         </li>
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,8 +1,84 @@
-import React from 'react'
+import {useState} from 'react'
+import { Link } from 'react-router-dom'
+import Icon from '@mdi/react'
+import { mdiMagnify, mdiBellRing, mdiArrowDown, mdiEye, mdiPencil } from '@mdi/js'
+import { useEffect } from 'react'
 
 const Admin = () => {
+
+  const userData = [
+    { ID: "#1", Name: "Emir Wicks", Email: "emir.wicks@mail.com", Verified: "Yes" },
+    { ID: "#2", Name: "Zaina Goldsmith", Email: "zaina.goldsmith@mail.com", Verified: "Yes" },
+    { ID: "#3", Name: "Mahima Lopez", Email: "mahima.lopez@mail.com", Verified: "Yes" },
+    { ID: "#4", Name: "Pharrell Murray", Email: "pharrell.murray@mail.com", Verified: "Yes" },
+    { ID: "#5", Name: "Annika Mcbride", Email: "annika.mcbride@mail.com", Verified: "Yes" },
+    { ID: "#6", Name: "Fatimah Clark", Email: "fatimah.clark@mail.com", Verified: "Yes" },
+    { ID: "#7", Name: "Klaudia Rhodes", Email: "klaudia.rhodes@mail.com", Verified: "Yes" },
+    { ID: "#8", Name: "Tillie Lucero", Email: "tillie.lucero@mail.com", Verified: "Yes" },
+    { ID: "#9", Name: "Sabrina Stephenson", Email: "sabrina.stephenson@mail.com", Verified: "Yes" },
+    { ID: "#10", Name: "Annie Smith", Email: "annie.smith@mail.com", Verified: "Yes" }
+  ]
+
+  // const [data, setData] = useState('');
+
+  // useEffect(()=>{
+  //   setData(userData)
+  // },[])
+
+
   return (
-    <div>Admin</div>
+    <>
+      <nav className="container row justify-content-between py-8">
+        <div className="col-6 d-flex align-items-center">
+          <h1 className="me-12">Admin</h1>
+          <p className="fs-6 fw-bold mb-0">
+            <Icon path={mdiMagnify} size={1} className="text-success me-2" />
+            Add New Admin
+          </p>
+        </div>
+        <div className="col-6 d-flex justify-content-end">
+          <Link className="me-6" to="">
+            <Icon path={mdiMagnify} size={1} className="text-dark" />
+          </Link>
+          <Link to="">
+            <Icon path={mdiBellRing} size={1} className="text-success" />
+          </Link>
+        </div>
+      </nav>
+      <section className="px-6 py-4">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">
+                ID
+                <Icon path={mdiArrowDown} size={1} className="text-success ms-2" />
+              </th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Verified</th>
+              <th scope="col"> </th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {userData.map((item,i) => (
+              <tr key={i}>
+                <th scope="row">{item.ID}</th>
+                <td>{item.Name}</td>
+                <td>{item.Email}</td>
+                <td>{item.Verified}</td>
+                <td>
+                  <Icon path={mdiEye} size={1} className="text-dark me-6" />
+                  <Icon path={mdiPencil} size={1} className="text-dark" />
+                </td>
+              </tr>
+            ))}
+
+          </tbody>
+        </table>
+      </section>
+
+    </>
   )
 }
 
